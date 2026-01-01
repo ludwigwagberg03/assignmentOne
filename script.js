7ec486de2f2204d80d4054dcec708faaa3c51559
ludwigwagberg03 <ludwig.wagberg@gmail.com>
2026-01-01 12:26:06 +0100

    adding a character object and creating a main function and moving the other functions out

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", main);
+
+function main() {
     let title = document.getElementById("title-text");
     let buttons = document.getElementById("buttons");
     let currentLocation = "forest";
+    let CharacterInfo = document.querySelector(".character-info");
 
     let world = {
         forest: {
@@ -26,29 +29,54 @@ document.addEventListener("DOMContentLoaded", (event) => {
         }
     };
 
-    function createButtons() {
-        buttons.innerHTML = "";
+    let character = {
+        name: "Player One",
+        hitPoints: 100,
+        inventory: ["sword", "axe"]
+    }
 
-        world[currentLocation].exits.forEach(element => {
-            let button = document.createElement("button");
-            button.classList.add("my-button");
-            button.innerHTML = "Go to " + element;
+    changeLocation(world, currentLocation, buttons, title);
+    renderCharacterInformation(character, CharacterInfo);
+}
 
-            button.addEventListener("click", function () {
-                currentLocation = element;
-                changeLocation();
-            })
-            buttons.appendChild(button);
-        });
-    };
+function renderCharacterInformation(character, CharacterInfo) {
+    createCharacterNameElement(character, CharacterInfo);
+    createCharacterHitPointsElement(character, CharacterInfo);
+}
+function createCharacterNameElement(character, CharacterInfo) {
+    let characterName = document.createElement("h4");
+    characterName.innerHTML = character.name;
 
-    function changeLocation() {
-        let data = world[currentLocation];
+    CharacterInfo.append(characterName);
+}
 
-        document.body.style.backgroundImage = data.image;
-        document.body.style.backgroundSize = "cover";
-        title.innerHTML = data.text;
-        createButtons();
-    };
-    changeLocation();
-});
+function createCharacterHitPointsElement(character, CharacterInfo) {
+    let characterHitPoints = document.createElement("h4");
+    characterHitPoints.innerHTML = character.hitPoints;
+
+    CharacterInfo.append(characterHitPoints);
+}
+function createButtons(world, currentLocation, buttons, title) {
+    buttons.innerHTML = "";
+
+    world[currentLocation].exits.forEach(element => {
+        let button = document.createElement("button");
+        button.classList.add("my-button");
+        button.innerHTML = "Go to " + element;
+
+        button.addEventListener("click", function () {
+            currentLocation = element;
+            changeLocation(world, currentLocation, buttons, title);
+        })
+        buttons.appendChild(button);
+    });
+};
+
+function changeLocation(world, currentLocation, buttons, title) {
+    let data = world[currentLocation];
+
+    document.body.style.backgroundImage = data.image;
+    document.body.style.backgroundSize = "cover";
+    title.innerHTML = data.text;
+    createButtons(world, currentLocation, buttons, title);
+};
